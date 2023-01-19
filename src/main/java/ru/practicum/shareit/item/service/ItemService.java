44e09800d1be336b7ce6255c29b908a2df1c6841
IPilin <64646736+IPilin@users.noreply.github.com>
2023-01-19 23:46:13 +0300

    add: ItemService done.

--- src/main/java/ru/practicum/shareit/item/service/ItemService.java
+++ src/main/java/ru/practicum/shareit/item/service/ItemService.java
@@ -6,8 +6,12 @@ import java.util.Collection;
 
 public interface ItemService {
     Collection<ItemDto> getAllUserItems(Long userId);
+
     ItemDto getItemById(Long itemId);
+
     ItemDto createItem(Long userId, ItemDto itemDto);
+
     ItemDto updateItem(Long userId, Long itemId, ItemDto itemDto);
+
     Collection<ItemDto> search(String text);
 }
